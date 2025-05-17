682f2b199a9cabfcb9ec02ec86d8890ca7e33fc4
yanliu1111 <liuyan0504@gmail.com>
2025-05-17 16:09:38 -0600

    feat: update learn

--- src/auth/routers.py
+++ src/auth/routers.py
@@ -1,7 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from .schemas import UserCreateModel
+from .service import UserService
+from src.db.main import get_session
+from sqlmodel.ext.asyncio.session import AsyncSession
 
 auth_router = APIRouter()
-
+user_service = UserService()
 @auth_router.post("/signup")
-async def create_user_account():
-    return {"message": "User signed up successfully"}
+async def create_user_account(
+    user_data: UserCreateModel,
+    session: AsyncSession = Depends(get_session)
+):
+    
+    email = user_data.email
+    user_exists = user_service.user_exists(email, session)
