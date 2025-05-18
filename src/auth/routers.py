e5514989d0856df86b3b9acaeabeaeb68d996c75
yanliu1111 <liuyan0504@gmail.com>
2025-05-18 10:30:26 -0600

    feat: update

--- src/auth/routers.py
+++ src/auth/routers.py
@@ -3,14 +3,25 @@ from .schemas import UserCreateModel
 from .service import UserService
 from src.db.main import get_session
 from sqlmodel.ext.asyncio.session import AsyncSession
+from fastapi import HTTPException, status
+
 
 auth_router = APIRouter()
 user_service = UserService()
-@auth_router.post("/signup")
+@auth_router.post(
+        "/signup",
+        response_model=UserCreateModel,)
 async def create_user_account(
     user_data: UserCreateModel,
     session: AsyncSession = Depends(get_session)
 ):
     
     email = user_data.email
-    user_exists = user_service.user_exists(email, session)
+    user_exists = await user_service.user_exists(email, session)
+    if user_exists:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="User already exists",
+        )
+    new_user = await user_service.create_user(user_data, session)
+    return new_user
