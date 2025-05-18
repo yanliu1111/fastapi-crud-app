1ba49d35040c2169d39a9407cb4fb9ba92d88ef8
yanliu1111 <liuyan0504@gmail.com>
2025-05-18 11:56:58 -0600

    fix: bug fix

--- src/auth/models.py
+++ src/auth/models.py
@@ -5,7 +5,7 @@ import sqlalchemy.dialects.postgresql as pg
 
 class User(SQLModel, table=True):
     __tablename__ = "users"
-    user_id: uuid.UUID = Field(sa_column=Column(
+    uid: uuid.UUID = Field(sa_column=Column(
         pg.UUID,
         nullable=False,
         primary_key=True,
@@ -15,7 +15,7 @@ class User(SQLModel, table=True):
     email: str
     first_name: str
     last_name: str
-    is_active: bool
+    is_active: bool = Field(default=True)
     is_verified: bool = Field(default=False)
     password_hash: str = Field(exclude=True)
     created_at: datetime = Field(sa_column = Column(pg.TIMESTAMP, default=datetime.now))
