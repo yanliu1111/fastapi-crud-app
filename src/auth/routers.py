6aa1cc2ab704ca4467e50ef01e6b056b15d67768
yanliu1111 <liuyan0504@gmail.com>
2025-05-20 00:49:48 -0600

    feat: built refresh token route

--- src/auth/routers.py
+++ src/auth/routers.py
@@ -5,7 +5,7 @@ from src.db.main import get_session
 from sqlmodel.ext.asyncio.session import AsyncSession
 from fastapi import HTTPException, status
 from .utils import create_access_token, decode_token, verify_password
-from datetime import timedelta
+from datetime import timedelta, datetime
 from fastapi.responses import JSONResponse
 from .dependencies import RefreshTokenBearer
 
@@ -65,4 +65,18 @@ async def login_users(login_data: UserLoginModel, session: AsyncSession = Depend
 
 @auth_router.get('/refresh_token')
 async def get_new_access_token(token_details: dict = Depends(RefreshTokenBearer())):
-   return {}
+    expiry_timestamp = token_details['exp']
+    #    print(expiry_timestamp)
+    if datetime.fromtimestamp(expiry_timestamp) > datetime.now():
+        new_access_token = create_access_token(user_data=token_details['user'])
+        return JSONResponse(
+            content={
+                "message": "New access token generated",
+                "access_token": new_access_token,
+            }
+        )
+
+    raise HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail="Invalide Or expired token",
+    )
