04b25464bbfb1c4b60c62dcf5f5921d6816ef3e6
yanliu1111 <liuyan0504@gmail.com>
2025-05-22 12:17:47 -0600

    feat: build relationship in both schema

--- src/auth/models.py
+++ src/auth/models.py
@@ -1,7 +1,10 @@
-from sqlmodel import SQLModel, Field, Column
+from sqlmodel import SQLModel, Field, Column, Relationship
 import uuid
 from datetime import datetime
 import sqlalchemy.dialects.postgresql as pg
+from typing import List
+from src.books import models
+
 
 class User(SQLModel, table=True):
     __tablename__ = "users"
@@ -21,6 +24,7 @@ class User(SQLModel, table=True):
     password_hash: str = Field(exclude=True)
     created_at: datetime = Field(sa_column = Column(pg.TIMESTAMP, default=datetime.now))
     updated_at: datetime = Field(sa_column = Column(pg.TIMESTAMP, default=datetime.now))
+    books: List['models.Book'] = Relationship(back_populates="user", sa_relationship_kwargs={'lazy': 'selectin'})
 
     def __repr__(self):
         return f"<User {self.username}>"
--- src/books/models.py
+++ src/books/models.py
@@ -1,8 +1,11 @@
-from sqlmodel import SQLModel, Field, Column
+from sqlmodel import SQLModel, Field, Column, Relationship
 import sqlalchemy.dialects.postgresql as pg
 from datetime import datetime
 import uuid
 from typing import Optional
+from src.auth import models
+
+
 class Book(SQLModel, table=True):
     __tablename__ = "books"
     uid: uuid.UUID = Field(sa_column=Column(
@@ -20,7 +23,7 @@ class Book(SQLModel, table=True):
     user_uid:Optional [uuid.UUID] = Field(default=None, foreign_key="users.uid")
     created_at: datetime = Field(sa_column = Column(pg.TIMESTAMP, default=datetime.now))
     updated_at: datetime = Field(sa_column = Column(pg.TIMESTAMP, default=datetime.now))
-
+    user: Optional['models.User'] = Relationship(back_populates="books")
     # __repr__ method to return a string representation of the Book object
     def __repr__(self):
         return f"<Book {self.title}>"
