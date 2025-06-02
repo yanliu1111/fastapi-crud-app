b02797c4379d33d8de1ce56ccc998c6987458761
yanliu1111 <liuyan0504@gmail.com>
2025-06-01 23:43:58 -0600

    feat: update

--- src/middleware.py
+++ src/middleware.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.requests import Request
 import time
-import logging
+from fastapi.responses import JSONResponse
 
 logger = logging.getLogger('uvicorn.access')
 logger.disabled = True  # Disable Uvicorn's default access log
@@ -15,6 +15,16 @@ def register_middleware(app: FastAPI):
         
         response = await call_next(request)
         process_time = time.time() - start_time
-        message = f"Request: {request.method} {request.url.path} - complete after{process_time}s"
+        message = f"Request: {request.client.host} {request.client.port} - {request.method} - {request.url.path} - complete after{process_time}s"
         print (message)
         return response
+    
+    @app.middleware("http")
+    async def authorization (request: Request, call_next):
+        if "Authorization" not in request.headers:
+            return JSONResponse(
+                status_code=401,
+                content={"detail": "Authorization header missing",
+                         "resolution":"Please provide a valid Authorization header."
+                         }
+            )
