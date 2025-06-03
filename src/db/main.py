f512f5a32d1c67a4818822fcaa4e307ed8a6363c
yanliu1111 <liuyan0504@gmail.com>
2025-06-02 19:00:33 -0600

    feat: middleware continue

--- src/db/main.py
+++ src/db/main.py
@@ -10,7 +10,7 @@ database_url = Config.DATABASE_URL.replace("postgresql://", "postgresql+asyncpg:
 # Create an asynchronous engine using the PostgreSQL URL from the config
 engine = create_async_engine(
     url=database_url,
-    echo=True
+    # echo=True # means that SQLAlchemy will log all the statements issued to the database, useful for debugging purposes
 )
 
 async def init_db():
